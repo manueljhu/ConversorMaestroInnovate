c34c4cb390babfe10afd84594aa5b70d9010e9ab
juanmacasgim <juanmariacastillogimenez@gmail.com>
2024-10-18 14:19:16 +0200

    DBFController

--- src/main/java/com/innovate/conversormaestro/datasource/DBFController.java
+++ src/main/java/com/innovate/conversormaestro/datasource/DBFController.java
@@ -3,7 +3,6 @@ package com.innovate.conversormaestro.datasource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
